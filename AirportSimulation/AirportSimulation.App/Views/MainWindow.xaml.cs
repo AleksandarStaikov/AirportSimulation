051c76e8b8f3f27a348e6d756caf7e1ef1738b9c
I380743 <n.zhmakin@student.fontys.nl>
2019-04-14 20:43:57 +0200

    merging

--- AirportSimulation/AirportSimulation.App/Views/MainWindow.xaml.cs
+++ AirportSimulation/AirportSimulation.App/Views/MainWindow.xaml.cs
@@ -1,4 +1,19 @@
-﻿namespace AirportSimulation.App.Views
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AirportSimulation.App.Views
 {
     using System.Windows;
 
@@ -7,6 +22,13 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= TextBox_GotFocus;
         }
     }
 }
