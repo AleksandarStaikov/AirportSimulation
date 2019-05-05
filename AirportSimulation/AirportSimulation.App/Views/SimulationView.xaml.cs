24b298586d885434fe05345aacb5d2930369007d
Nikolas Zhmakin <nikolas.zhmakin@student.fontys.nl>
2019-05-05 13:52:30 +0200

    blaa

--- AirportSimulation/AirportSimulation.App/Views/SimulationView.xaml.cs
+++ AirportSimulation/AirportSimulation.App/Views/SimulationView.xaml.cs
@@ -24,5 +24,40 @@ namespace AirportSimulation.App.Views
         {
             InitializeComponent();
         }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            int selectedColumnIndex = -1, selectedRowIndex = -1;
+            var grid = sender as Grid;
+            if (grid != null)
+            {
+                var pos = e.GetPosition(grid);
+                var temp = pos.X;
+                for (var i = 0; i < grid.ColumnDefinitions.Count; i++)
+                {
+                    var colDef = grid.ColumnDefinitions[i];
+                    temp -= colDef.ActualWidth;
+                    if (temp <= -1)
+                    {
+                        selectedColumnIndex = i;
+                        break;
+                    }
+                }
+
+                temp = pos.Y;
+                for (var i = 0; i < grid.RowDefinitions.Count; i++)
+                {
+                    var rowDef = grid.RowDefinitions[i];
+                    temp -= rowDef.ActualHeight;
+                    if (temp <= -1)
+                    {
+                        selectedRowIndex = i;
+                        break;
+                    }
+                }
+            }
+
+            MessageBox.Show(selectedColumnIndex + ", " + selectedRowIndex);
+        }
     }
 }
