984df10b3db6cfcb70dbc0dca70057077a8e05d5
Aleksandar Staykov <a.staykov@student.fontys.nl>
2019-05-11 13:28:41 +0200

    added PickUpArea

---
+++ AirportSimulation/AirportApplication.Core/LinkNodes/PickUpArea.cs
@@ -0,0 +1,30 @@
+﻿namespace AirportSimulation.Core.LinkNodes
+{
+    using Abstractions.Contracts;
+    using Abstractions.Core;
+    using Common.Models;
+
+    public class PickUpArea : ProcessingNode
+    {
+        private const int DefaultPuckUpTimeInMilliseconds = 800;
+
+        private readonly int _pickUpRate;
+
+        public PickUpArea(ITimerService timerService, int pickUpRate = 0) : base(timerService)
+        {
+            if (pickUpRate == 0)
+            {
+                _pickUpRate = DefaultPuckUpTimeInMilliseconds;
+            }
+            else
+            {
+                _pickUpRate = pickUpRate;
+            }
+        }
+
+        public override void Process(Baggage baggage)
+        {
+            baggage.AddEventLog(TimerService.ConvertMillisecondsToTimeSpan(_pickUpRate), "PickUpArea processings");
+        }
+    }
+}
