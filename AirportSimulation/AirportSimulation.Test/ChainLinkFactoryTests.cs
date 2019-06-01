2929ed4a97de112717a44073ab06c50f258dfbe6
Aleksandar Staykov <a.staykov@student.fontys.nl>
2019-06-01 14:21:03 +0200

    added chain link factory tests and chain link node test

--- AirportSimulation/AirportSimulation.Test/ChainLinkFactoryTests.cs
+++ AirportSimulation/AirportSimulation.Test/ChainLinkFactoryTests.cs
@@ -27,26 +27,219 @@
                     NextNodes = null,
                     Type = BuildingComponentType.CheckIn
                 };
-
                 var simulationSettings = new SimulationSettings()
                 {
                     Nodes = new List<NodeCreationData>() { nodeData }
                 };
 
-
-                var resultNode =  sut.CreateChainLink(nodeData, simulationSettings);
+                var resultNode = sut.CreateChainLink(nodeData, simulationSettings);
 
                 resultNode.ShouldBeOfType<CheckInDesk>();
             }
         }
 
-        //private ChainLinkFactory InitiateChainLinkFactory()
-        //{
-        //    var checkInMock = new MockFactory<>();
+        [Test]
+        public void ChainLinkFactory_ShouldReturnConveyor_Correctly()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // The AutoMock class will inject a mock IDependency
+                // into the SystemUnderTest constructor
+                var sut = mock.Create<ChainLinkFactory>();
+                var nodeData = new NodeCreationData()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    NextNodes = null,
+                    Type = BuildingComponentType.Conveyor
+                };
+                var simulationSettings = new SimulationSettings()
+                {
+                    Nodes = new List<NodeCreationData>() { nodeData }
+                };
 
-        //    var factory = new ChainLinkFactory();
+                var resultNode = sut.CreateChainLink(nodeData, simulationSettings);
 
-        //    return factory;
-        //}
+                resultNode.ShouldBeOfType<OneToOneConveyor>();
+            }
+        }
+
+        [Test]
+        public void ChainLinkFactory_ShouldReturnManyToOneConveyor_Correctly()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // The AutoMock class will inject a mock IDepsendency
+                // into the SystemUnderTest constructor
+                var sut = mock.Create<ChainLinkFactory>();
+                var nodeData = new NodeCreationData()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    NextNodes = null,
+                    Type = BuildingComponentType.ManyToOneConveyor
+                };
+                var simulationSettings = new SimulationSettings()
+                {
+                    Nodes = new List<NodeCreationData>() { nodeData }
+                };
+
+                var resultNode = sut.CreateChainLink(nodeData, simulationSettings);
+
+                resultNode.ShouldBeOfType<ManyToOneConveyor>();
+            }
+        }
+
+        [Test]
+        public void ChainLinkFactory_ShouldReturnPsc_Correctly()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // The AutoMock class will inject a mock IDepsendency
+                // into the SystemUnderTest constructor
+                var sut = mock.Create<ChainLinkFactory>();
+                var nodeData = new NodeCreationData()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    NextNodes = null,
+                    Type = BuildingComponentType.PSC
+                };
+                var simulationSettings = new SimulationSettings()
+                {
+                    Nodes = new List<NodeCreationData>() { nodeData }
+                };
+
+                var resultNode = sut.CreateChainLink(nodeData, simulationSettings);
+
+                resultNode.ShouldBeOfType<Psc>();
+            }
+        }
+
+        [Test]
+        public void ChainLinkFactory_ShouldReturnAsc_Correctly()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // The AutoMock class will inject a mock IDepsendency
+                // into the SystemUnderTest constructor
+                var sut = mock.Create<ChainLinkFactory>();
+                var nodeData = new NodeCreationData()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    NextNodes = null,
+                    Type = BuildingComponentType.ASC
+                };
+                var simulationSettings = new SimulationSettings()
+                {
+                    Nodes = new List<NodeCreationData>() { nodeData }
+                };
+
+                var resultNode = sut.CreateChainLink(nodeData, simulationSettings);
+
+                resultNode.ShouldBeOfType<Asc>();
+            }
+        }
+
+
+        [Test]
+        public void ChainLinkFactory_ShouldReturnMpa_Correctly()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // The AutoMock class will inject a mock IDepsendency
+                // into the SystemUnderTest constructor
+                var sut = mock.Create<ChainLinkFactory>();
+                var nodeData = new NodeCreationData()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    NextNodes = null,
+                    Type = BuildingComponentType.MPA
+                };
+                var simulationSettings = new SimulationSettings()
+                {
+                    Nodes = new List<NodeCreationData>() { nodeData }
+                };
+
+                var resultNode = sut.CreateChainLink(nodeData, simulationSettings);
+
+                resultNode.ShouldBeOfType<Mpa>();
+            }
+        }
+
+
+        [Test]
+        public void ChainLinkFactory_ShouldReturnBsu_Correctly()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // The AutoMock class will inject a mock IDepsendency
+                // into the SystemUnderTest constructor
+                var sut = mock.Create<ChainLinkFactory>();
+                var nodeData = new NodeCreationData()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    NextNodes = null,
+                    Type = BuildingComponentType.BSU
+                };
+                var simulationSettings = new SimulationSettings()
+                {
+                    Nodes = new List<NodeCreationData>() { nodeData }
+                };
+
+                var resultNode = sut.CreateChainLink(nodeData, simulationSettings);
+
+                resultNode.ShouldBeOfType<BSU>();
+            }
+        }
+
+
+        [Test]
+        public void ChainLinkFactory_ShouldReturnAa_Correctly()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // The AutoMock class will inject a mock IDepsendency
+                // into the SystemUnderTest constructor
+                var sut = mock.Create<ChainLinkFactory>();
+                var nodeData = new NodeCreationData()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    NextNodes = null,
+                    Type = BuildingComponentType.AA
+                };
+                var simulationSettings = new SimulationSettings()
+                {
+                    Nodes = new List<NodeCreationData>() { nodeData }
+                };
+
+                var resultNode = sut.CreateChainLink(nodeData, simulationSettings);
+
+                resultNode.ShouldBeOfType<Aa>();
+            }
+        }
+
+
+        [Test]
+        public void ChainLinkFactory_ShouldReturnPua_Correctly()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // The AutoMock class will inject a mock IDepsendency
+                // into the SystemUnderTest constructor
+                var sut = mock.Create<ChainLinkFactory>();
+                var nodeData = new NodeCreationData()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    NextNodes = null,
+                    Type = BuildingComponentType.PA
+                };
+                var simulationSettings = new SimulationSettings()
+                {
+                    Nodes = new List<NodeCreationData>() { nodeData }
+                };
+
+                var resultNode = sut.CreateChainLink(nodeData, simulationSettings);
+
+                resultNode.ShouldBeOfType<PickUpArea>();
+            }
+        }
     }
 }
---
+++ AirportSimulation/AirportSimulation.Test/ChainLinkNodeTests.cs
@@ -0,0 +1,31 @@
+﻿namespace AirportSimulation.Test
+{
+    using System;
+    using Abstractions.Contracts;
+    using Common.Models;
+    using Core.LinkNodes;
+    using Moq;
+    using NUnit.Framework;
+    using Shouldly;
+
+    [TestFixture]
+    public class ChainLinkNodeTests
+    {
+        [Test]
+        public void ChainLinkNode_ShouldAdd_ProcessingLog()
+        {
+            var timerMock = new Mock<ITimerService>();
+            timerMock.Setup(time => time.SimulationMultiplier).Returns(1);
+            var conveyorMock = new Mock<OneToOneConveyor>(10, Guid.NewGuid().ToString(), timerMock.Object);
+            conveyorMock.SetupGet(conv => conv.Destination).Returns("Psc");
+            var bag = new Baggage();
+
+            var node = new CheckInDesk(Guid.NewGuid().ToString(), timerMock.Object);
+            node.AddSuccessor(conveyorMock.Object);
+
+            node.PassBaggage(bag);
+
+            bag.Log.Count.ShouldBe(1);
+        }
+    }
+}
